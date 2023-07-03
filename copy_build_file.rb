903e304a2cab2a02694197b83126db95c9adb8ea
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-07-03 13:47:20 +0900

    Update usage of YAML.safe_load

--- copy_build_file.rb
+++ copy_build_file.rb
@@ -12,7 +12,7 @@ end
 
 config_path = File.join(config_dir, "config.yml")
 
-conf = YAML.safe_load(File.read(config_path), [Date])
+conf = YAML.safe_load(File.read(config_path), permitted_classes: [Date])
 epub_path = File.join(config_dir, conf["bookname"]+".epub")
 puts "generate EPUB #{epub_path}."
 
